9e60e0b30ea08285f93d0f926e333568a452b716
Ashwin Mohan Ram <ashwin@prime7solutions.com>
2018-04-08 03:09:49 +0800

    Tweak leaderboard header bar

--- resources/views/leaderboard/index.blade.php
+++ resources/views/leaderboard/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="my-10"><h1>Leaderboard</h1></div>
+    <div class="mt-10 bg-green-lightest border-l-4 border-green text-blue-dark p-4"><h1>Leaderboard</h1></div>
     <leaderboard></leaderboard>
 @endsection
