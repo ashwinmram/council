bbd7fbf2d0d571d9551523b11de88bd7b13864fe
Ashwin Mohan Ram <ashwin@prime7solutions.com>
2018-04-08 15:02:39 +0800

    Change confirm email verbiage

--- resources/views/emails/confirm-email.blade.php
+++ resources/views/emails/confirm-email.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # One Last Step
 
-We just need you to confirm your email address to prove that you're a human. You get it, right? Coo.
+We just need you to confirm your email address to prove that you're a human.
 
 @component('mail::button', ['url' => url('/register/confirm?token=' . $user->confirmation_token)])
 Confirm Email
